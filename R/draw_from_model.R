5f283eec98d7841a4f012b93d957e0a0504c02d8
Nick Strayer <nick.strayer@gmail.com>
2019-10-24 22:30:57 +0000

    Added the draw-target lambdas to the output of model draw for use in visualizing results side by side with set structure.

--- R/draw_from_model.R
+++ R/draw_from_model.R
@@ -5,11 +5,17 @@
 #' returns that data as a datafrom of each pair and the total number of
 #' connection seen for that pair.
 #'
-#' @param b_a Group membership vector for the a nodes. See \code{\link{assign_group_membership()}} for more details on format.
+#' @param b_a Group membership vector for the a nodes. See
+#'   \code{\link{assign_group_membership()}} for more details on format.
 #' @param b_b Group membership vectof for the b nodes
-#' @param Lambda Tibble containing average number of connections for each unique pair of a and b node groups. See \code{\link{generate_random_lambda()}} for more details.
+#' @param Lambda Tibble containing average number of connections for each unique
+#'   pair of a and b node groups. See \code{\link{generate_random_lambda()}} for
+#'   more details.
 #'
-#' @return A tibble with colums for ids of each node pair (`a, b`), the groups those node are in (`a_group, b_group`) and the number of edges between those two pairs from model draw (`num_edges`).
+#' @return A tibble with colums for ids of each node pair (`a, b`), the groups
+#'   those node are in (`a_group, b_group`), (`avg_num_cons`) for the expected
+#'   number of connections from the `Lambda` matrix and the number of edges
+#'   between those two pairs from model draw (`num_edges`).
 #' @export
 #'
 #' @examples
@@ -26,13 +32,13 @@ draw_from_model <- function(b_a, b_b, Lambda){
 
   # Grab the group membership for each node and get avg connections for each
   # pair by joining with Lambda matrix
-  pair_groups <- dplyr::inner_join(
-    node_pairs[c("a_group", "b_group")],
+  node_pairs <- dplyr::inner_join(
+    node_pairs,
     Lambda,
     by = c('a_group' = 'a', 'b_group' = 'b')
   )
 
   # Draw from Poison distribution with given average for each pair
-  node_pairs$num_edges <- rpois(n_pairs, lambda = pair_groups$avg_num_cons)
+  node_pairs$num_edges <- rpois(n_pairs, lambda = node_pairs$avg_num_cons)
   as_tibble(node_pairs)
 }
