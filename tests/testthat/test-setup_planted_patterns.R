0c7e26c5b31a87cfd4f7ac3e8d04550e65f383c5
Nick Strayer <nick.strayer@gmail.com>
2019-10-24 22:42:06 +0000

    Setup a skeleton for planted pattern generation tests.

---
+++ tests/testthat/test-setup_planted_patterns.R
@@ -0,0 +1,14 @@
+context("Planted pattern model generation")
+
+test_that("Lambda is the right size", {
+
+})
+
+test_that("Noise code lambdas are properly reflected", {
+
+})
+
+test_that("Makes sure on lambdas are higher than off lambdas", {
+
+})
+
