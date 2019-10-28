163cf6420ef77a04b341317164a83626671d7272
Nick Strayer <nick.strayer@gmail.com>
2019-10-28 15:48:04 +0000

    Updated site build script

--- tools/build_site.R
+++ tools/build_site.R
@@ -4,12 +4,12 @@ pkgdown::build_site(preview = FALSE)
 # Deploy package site to RStudio connect
 site_loc <- here::here('docs/')
 rsconnect::deployApp(
-  site_loc,                          # the directory containing the content
+  site_loc,                                          # the directory containing the content
   appFiles = list.files(site_loc, recursive = TRUE), # the list of files to include as dependencies (all of them)
-  appPrimaryDoc = "index.html",                 # the primary file
+  appPrimaryDoc = "index.html",                      # the primary file
   appName = "biSBMSim_Package",                      # name of the endpoint (unique to your account on Connect)
-  appTitle = "biSBMSim Package",                  # display name for the content
-  account = 'nstrayer',                # your Connect username
-  server = 'prod.tbilab.org'                     # the Connect server, see rsconnect::accounts()
+  appTitle = "biSBMSim Package",                     # display name for the content
+  account = 'nstrayer',                              # your Connect username
+  server = 'prod.tbilab.org'                         # the Connect server, see rsconnect::accounts()
 )
 
